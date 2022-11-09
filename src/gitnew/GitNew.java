9628cdc6c681cb781420ec211e4bb719d7b8d1e9
mahboobullahatiqi <117614573+mahboobullahatiqi@users.noreply.github.com>
2022-11-08 17:31:40 -0800

    Update GitNew.java
    commit by mahboob atiqi

--- src/gitnew/GitNew.java
+++ src/gitnew/GitNew.java
@@ -1,10 +1,13 @@
 package gitnew;
-
+import java.util.ArrayList;
 public class GitNew {
 	public static void main(String[] args) {
 		int x = 2;
 		int y= 4;
 		System.out.println(x*y);
+		
+		ArrayList<String> men = new ArrayList<>();
+		men.add(0,"ahmad");
 		
 		}
 
